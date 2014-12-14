83e19972cd2793534edaaaf0c72845455e26d2fd
litao <953521476@qq.com>
2014-12-15 03:37:43 +0800

    The first one
    第一个超级简单的openGL

---
+++ console/first_OpennGL.cpp
@@ -0,0 +1,21 @@
+﻿#include <glut.h>
+
+void myDisplay(void)
+{
+
+     glClear(GL_COLOR_BUFFER_BIT);
+     glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
+     glFlush();
+}
+
+int main(int argc, char *argv[])
+{
+     glutInit(&argc, argv);
+     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+     glutInitWindowPosition(100, 100);
+     glutInitWindowSize(400, 400);
+     glutCreateWindow("第一个OpenGL程序");
+     glutDisplayFunc(&myDisplay);
+     glutMainLoop();
+     return 0;
+}
